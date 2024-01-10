e06615de91f1d5aa0f95cdd54e1d2d9212d2da14
cheenar <cheenar@hey.com>
2024-01-10 04:21:08 -0800

    [basic] fixing more imports

--- src/Winx.cpp
+++ src/Winx.cpp
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "libplatform/libplatform.h"
+#include <libplatform/libplatform.h>
 #include <v8.h>
 #include "bindings/winx_console.hpp"
 #include "bindings/winx_fs.hpp"
