e0e24e29beb7ec5a4a7e8686cd649bbc86e0f170
cheenar <cheenar@hey.com>
2024-01-23 01:09:48 -0800

    [refactor] winx_console.hpp

--- src/winx_console.hpp
+++ src/winx_console.hpp
@@ -1,5 +1,5 @@
-#ifndef WINX_CONSOLE_HPP
-#define WINX_CONSOLE_HPP
+#ifndef __SRC_WINX_CONSOLE_HPP__
+#define __SRC_WINX_CONSOLE_HPP__
 
 #include <v8.h>
 
@@ -12,28 +12,38 @@
 
 extern bool IS_DEBUG_MODE_ENABLED;
 
+/**
+ * @brief Produces a formatted print output with debug flags enabled.
+ *
+ * @param args The function callback args.
+ * @param prefix The prefix to print before the message.
+ */
 static void formatted_print(const v8::FunctionCallbackInfo<v8::Value> &args, std::string prefix);
 
 namespace Winx::Bindings::Console
 {
 
-void log(const v8::FunctionCallbackInfo<v8::Value> &args)
-{
-    formatted_print(args, "log");
-}
+/**
+ * @brief Produces a stdout message.
+ *
+ * @param args The function callback args.
+ */
+void log(const v8::FunctionCallbackInfo<v8::Value> &args);
 
-void debug(const v8::FunctionCallbackInfo<v8::Value> &args)
-{
-    formatted_print(args, "debug");
-}
+/**
+ * @brief Produces a stdout message with debug headers.
+ *
+ * @param args The function callback args.
+ */
+void debug(const v8::FunctionCallbackInfo<v8::Value> &args);
 
-v8::Local<v8::ObjectTemplate> EngineBind(v8::Isolate *isolate)
-{
-    v8::Local<v8::ObjectTemplate> console = Winx::Binding::create_winx_object_binding(isolate);
-    Winx::Binding::create_winx_function_binding(isolate, console, "log", log);
-    Winx::Binding::create_winx_function_binding(isolate, console, "debug", debug);
-    return console;
-}
+/**
+ * @brief Binds the methods to the V8 engine.
+ *
+ * @param isolate The v8 isolate.
+ * @return v8::Local<v8::ObjectTemplate> The object template to bind to the engine.
+ */
+v8::Local<v8::ObjectTemplate> EngineBind(v8::Isolate *isolate);
 
 } // namespace Winx::Bindings::Console
 
@@ -57,4 +67,22 @@ static void formatted_print(const v8::FunctionCallbackInfo<v8::Value> &args, std
     }
 }
 
-#endif // WINX_CONSOLE_HPP
+void Winx::Bindings::Console::log(const v8::FunctionCallbackInfo<v8::Value> &args)
+{
+    formatted_print(args, "log");
+}
+
+void Winx::Bindings::Console::debug(const v8::FunctionCallbackInfo<v8::Value> &args)
+{
+    formatted_print(args, "debug");
+}
+
+v8::Local<v8::ObjectTemplate> Winx::Bindings::Console::EngineBind(v8::Isolate *isolate)
+{
+    v8::Local<v8::ObjectTemplate> console = Winx::Binding::create_winx_object_binding(isolate);
+    Winx::Binding::create_winx_function_binding(isolate, console, "log", log);
+    Winx::Binding::create_winx_function_binding(isolate, console, "debug", debug);
+    return console;
+}
+
+#endif // __SRC_WINX_CONSOLE_HPP__
